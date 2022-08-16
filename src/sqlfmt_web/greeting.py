eea5423bf921a3cf9d4347032ac2295efeac35eb
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-08-16 11:53:30 -0600

    feat: add button to load example query, refactor greeting

---
+++ src/sqlfmt_web/greeting.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+GREETING = """
+# sqlfmt
+
+sqlfmt formats your dbt SQL files so you don't have to. It is similar in nature to black, gofmt, and rustfmt (but for SQL).
+
+When you use sqlfmt:
+
+1.  You never have to mention (or argue about) code style in code reviews again
+1.  You make it easier to collaborate and solicit contributions from new people
+1.  You will be able to read your team's code as if you wrote it
+1.  You can forget about formatting your code, and spend your time on business logic instead
+
+## Installing sqlfmt
+
+**tl;dr: `pip install shandy-sqlfmt[jinjafmt]`**
+
+sqlfmt is a command-line tool that is built in Python and runs on MacOS, Linux, and Windows. Visit the **[Github Repo](https://github.com/tconbeer/sqlfmt)** for more information on how to get started.
+
+## Or you can try it out here
+
+Type or paste SQL into the box below, then click the **sqlfmt!** button to see your code in the sqlfmt style.
+"""
